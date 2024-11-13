1e190a5e072fb487eea93d004b2ec75f4aa875c0
Magdalena Jadach <magdalena.jadach@gmail.com>
2024-11-13 11:31:55 +0000

    Terminal style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
 import Header from './components/Header/Header';
+import Terminal from './components/Terminal/Terminal';
 
 function App() {
   return (
     <div>
       <div className="App">
         <Header />
+        <Terminal />
       </div>
     </div>
   );
