aadeb8381fd1873b83296cd46e958085efa20d12
Magdalena Jadach <mjadach@ip-192-168-1-6.eu-west-2.compute.internal>
2025-11-12 10:12:54 +0000

    Replace assets

--- src/app/components/ProjectsSection/projectsData.js
+++ src/app/components/ProjectsSection/projectsData.js
@@ -4,22 +4,30 @@
  */
 
 export const projectsData = [
+    {
+    title: "Medical Tracker",
+    description:
+      "The Medical Tracker is a web application designed to help users manage their health data effectively. Built with React and Node.js, it offers features like medication tracking, appointment scheduling, and health reminders.",
+    url: "https://www.medicaltracker.co.uk/",
+    imageUrl: "/projects/mtl.png",
+    imageAlt: "Screenshot of Medical Tracker application showing user dashboard",
+  },
   {
     title: "Code Editor",
     description:
       "The Raspberry Pi Code Editor is a browser-based platform designed to help young learners explore coding in an intuitive and interactive environment. Built with React for a responsive user interface and CodeMirror for powerful code editing functionality, it supports HTML and Python projects.",
     url: "https://editor.raspberrypi.org/en",
-    imageUrl: "/projects/editor2.png",
+    imageUrl: "/projects/editor.png",
     imageAlt: "Screenshot of Raspberry Pi Code Editor interface showing code editing capabilities",
   },
-  {
-    title: "Weather App",
-    description:
-      "Real-time weather forecasts using React and OpenWeatherMap API. Provides current conditions, hourly and 5-day forecasts, with an intuitive and responsive design.",
-    url: "https://www.weather.jadach.dev",
-    imageUrl: "/projects/weather.png",
-    imageAlt: "Screenshot of Weather App displaying current weather conditions and forecast",
-  },
+  // {
+  //   title: "Weather App",
+  //   description:
+  //     "Real-time weather forecasts using React and OpenWeatherMap API. Provides current conditions, hourly and 5-day forecasts, with an intuitive and responsive design.",
+  //   url: "https://www.weather.jadach.dev",
+  //   imageUrl: "/projects/weather.png",
+  //   imageAlt: "Screenshot of Weather App displaying current weather conditions and forecast",
+  // },
   {
     title: "Projects Site",
     description:
